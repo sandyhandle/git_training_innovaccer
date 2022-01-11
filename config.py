acdec049684b85e0341b5a00772b875d1793a3e3
sandyhandle <64911148+sandyhandle@users.noreply.github.com>
2022-01-11 12:36:13 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+class Config:
+    CHROME_EXECUTABLE_PATH = "/usr/local/bin/chromedriver"
+    PROJECT_BASE_URL = "https://www.saucedemo.com/"
+
+    USER_NAME = "standard_user"
+    USER_PASSWORD = "secret_sauce"
+    
+    LOGIN_TITLE = "Swag Labs"
+    
+    INVENTRY_URL = "https://www.saucedemo.com/inventory.html"
+
+
+
